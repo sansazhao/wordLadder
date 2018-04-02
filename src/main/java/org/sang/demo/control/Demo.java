efbcdc443a7379c7efaedec5a6503b914ad2b1f3
zhaoying98sjtu <zhaoying98@sjtu.edu.cn>
2018-04-03 01:03:43 +0800

    first commit

---
+++ src/main/java/org/sang/demo/control/Demo.java
@@ -0,0 +1,25 @@
+package org.sang.demo.control;
+
+public class Demo {
+    private String name;
+    private String password;
+
+    public Demo() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
